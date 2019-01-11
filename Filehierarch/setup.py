8c7b6b5af7f4fe2bdb71a780682a44df0308c2b3
timob <bemmer.timo@gmail.com>
2019-01-11 13:27:00 +0100

    Added setup.py.

---
+++ Filehierarch/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+
+setup(name='filehierarch',
+     version='1.0',
+     description='A simpole filetree printer',
+     author='isaactfa',
+     author_email='isaacthefallenapple@gmail.com',
+     packages=['filehierarch'])
+
